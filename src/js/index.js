9d52ed240120625d04bca438d9b60e981bea59d7
Frankie Bukenya <frankie.bukenya@gmail.com>
2019-05-20 20:41:17 +0300

    Add JS and Index pages

---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+// jshint esversion: 6
+window.addEventListener('keydown', function(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  if (!audio) return; // Stop the function from running
+  audio.play();
+});
