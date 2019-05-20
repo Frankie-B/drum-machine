d9229f370ab5a98b20945bfc23e76aad309e91dd
Frankie Bukenya <frankie.bukenya@gmail.com>
2019-05-20 21:06:08 +0300

    Fix Background

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,9 @@
 // jshint esversion: 6
 window.addEventListener('keydown', function(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
   if (!audio) return; // Stop the function from running
+  audio.currentTime = 0; // Rewind to the start
   audio.play();
+  key.classList.add('playing');
 });
